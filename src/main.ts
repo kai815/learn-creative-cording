81673c3f116feba2e974db9e2a7256d92680a93b
kai815 <h.kaizuka815@gmail.com>
2021-12-05 12:02:06 +0900

    スタイルの修正と試しに丸を書く

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("Hello p5!");
+import p5 from "p5";
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.circle(p.width / 2, p.height / 2, 50);
+  };
+};
+
+new p5(sketch);
