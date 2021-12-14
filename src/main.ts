c4c122cb47fe3fc95582e87e67d7947d710f5186
kai815 <h.kaizuka815@gmail.com>
2021-12-14 21:36:05 +0900

    楕円を利用したかっこいい模様

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import p5 from "p5";
 const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noFill()
   };
   p.draw = () => {
-    p.strokeWeight(p.random(1,5))
     p.stroke(p.random(180,255))
-    p.circle(p.width / 2 , p.height /2 ,p.random(50,300))
+    p.ellipse(p.random(p.width), p.height /2 , p.random(50,300), p.random(50,300))
   }
 };
 
