3dc48e9cdaf4e90a7cb54fda5a7578d4d6af3c44
kai815 <h.kaizuka815@gmail.com>
2023-07-05 07:36:41 +0900

    👔 maxとminを組み合わせる

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
 import p5 from "p5";
 
-let x,y;
+let left,right;
 const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    x = 0;
-    y = p.height / 2;
+    p.noFill();
+  
+    left = 100;
+    right = p.width - 100;
   };
 
   p.draw = () => {
     p.clear();
 
-    // 中央から左上の距離
-    const max = p.dist(0, 0, p.width / 2, p.height / 2); //distは距離を計算する
-    // 中央からマウスの位置
-    const d = p.dist(p.width / 2, p.height / 2, p.mouseX, p.mouseY);
-    p.circle(p.mouseX, p.mouseY, map(d, 0, max, max, 0));
+    p.strokeWeight(1);
+    p.stroke(255, 0, 0);
+    p.line(left, 0, left, p.height);//左の線
+    p.line(right, 0, right, p.height);//右の線
+  
+    p.strokeWeight(2);
+    p.stroke(240);
+    //maxとminを組み合わせる
+    const mx = p.max(left, p.min(right, p.mouseX));
+    p.line(mx, 0, mx, p.height);
   }
 };
 new p5(sketch);
@@ -36,4 +43,9 @@ const lerp = (a:number, b:number, t:number) => {
 // v:範囲Aの値,a:範囲Aの下限,b:範囲Aの上限,c:下限Bの下限 d:範囲Bの上限,
 const map = (v:number, a:number, b:number, c:number, d:number) => {
   return lerp(c, d, norm(v, a, b));
+}
+
+// max() と min() の組み合わせをクランプ値が最小値～最大値の範囲を超えないようにする
+const clamp = (value, min, max) => {
+  return max(min(value, max), min);
 }
